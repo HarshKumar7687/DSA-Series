55e941249a070e8d39d94cbb9fedf82a8d1c7315
HarshKumar7687 <drharshkumar499@gmail.com>
2025-12-16 19:18:21 +0530

    sort

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,6 +1,4 @@
 class bubble_sort {
-    
-    // An optimized version of Bubble Sort
     static void bubbleSort(int arr[], int n){
         int i, j, temp;
         boolean swapped;
@@ -8,23 +6,17 @@ class bubble_sort {
             swapped = false;
             for (j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    
-                    // Swap arr[j] and arr[j+1]
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
                 }
             }
-
-            // If no two elements were
-            // swapped by inner loop, then break
             if (swapped == false)
                 break;
         }
     }
 
-    // Function to print an array
     static void printArray(int arr[], int size){
         int i;
         for (i = 0; i < size; i++)
@@ -32,7 +24,6 @@ class bubble_sort {
         System.out.println();
     }
 
-    // Driver program
     public static void main(String args[]){
         int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
         int n = arr.length;
