f56b63100af33fd459200909f9c8b10510fcb67a
HarshKumar7687 <drharshkumar499@gmail.com>
2025-12-16 22:20:22 +0530

    lastff

--- day 1-2/merged_sort.java
+++ day 1-2/merged_sort.java
@@ -5,9 +5,9 @@ class merged_sort {
         int L[] = new int[n1];
         int R[] = new int[n2];
 
-        for (int i = 0; i < n1; ++i)
+        for (int i = 0; i < n1; i++)
             L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
+        for (int j = 0; j < n2; j++)
             R[j] = arr[m + 1 + j];
 
         int i = 0, j = 0;
