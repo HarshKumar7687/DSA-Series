6a37da713ca72b89c21bbc039ddeb588504d5f48
HarshKumar7687 <drharshkumar499@gmail.com>
2025-12-20 20:01:58 +0530

    ..

--- day 2-3/anton_and_letters.java
+++ day 2-3/anton_and_letters.java
@@ -7,7 +7,6 @@ public class anton_and_letters {
 
         boolean[] seen = new boolean[26];
         int count = 0;
-
         for (char c : s.toCharArray()) {
             if (c >= 'a' && c <= 'z') {
                 if (!seen[c - 'a']) {
