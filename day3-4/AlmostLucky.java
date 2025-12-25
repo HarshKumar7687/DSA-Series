b3bc862db521aa33dd32762c21801162c3f1ac7e
HarshKumar7687 <drharshkumar499@gmail.com>
2025-12-25 23:42:40 +0530

    co

--- day3-4/AlmostLucky.java
+++ day3-4/AlmostLucky.java
@@ -5,11 +5,7 @@ public class AlmostLucky {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[] luckyNumbers = {
-            4, 7, 44, 47, 74, 77,
-            444, 447, 474, 477,
-            744, 747, 774, 777
-        };
+        int[] luckyNumbers = {4, 7, 44, 47, 74, 77,444, 447, 474, 477,744, 747, 774, 777};
 
         for (int lucky : luckyNumbers) {
             if (n % lucky == 0) {
